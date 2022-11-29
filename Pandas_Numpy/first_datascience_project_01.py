cf52aa5f170e2c75c9f172a7ee07fa8fcde6658f
ManSzeLam109021153 <116985515+ManSzeLam109021153@users.noreply.github.com>
2022-11-29 17:23:56 +0800

    Add files via upload

---
+++ Pandas_Numpy/first_datascience_project_01.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import csv
+#Open csv file up
+with open ('t187ap03_L.csv') as file:
+    file_01 = csv.reader(file)
+    for row in file_01:
+        print(row)
+#read the csv file
+park_1 = pd.read_csv('t187ap03_L.csv')
+#find the headers you want to shown 
+park_1['公司名稱']
+company_name = park_1.公司名稱
+park_1['住址']
+address=park_1.住址
+park_1['上市日期']
+listing_date=park_1.上市日期
+park_1['實收資本額']
+Paid_in_capital = park_1.實收資本額
+#Print any types of data in csv file
+print('公司名稱',company_name)
+print('公司住址',address)
+print('上市日期',listing_date)
+print('實收資本額',Paid_in_capital)
+
+#to add those city up
+#if address == '台北' or address == '臺北':
+    #address.count(address)
+#add those data to SQL
+#Using those data and show it on Tkinter
